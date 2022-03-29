d2b76b5af574f1ad931cd27a63abaa0925db23db
aravindan <aravindan.acct@gmail.com>
2022-03-29 22:20:45 +0530

    customscript updated

--- scripts/customScript.sh
+++ scripts/customScript.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Install VirtualBox hypervisor
-<< comment
+
 sudo apt-get update
 sudo apt remove virtualbox*
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
@@ -9,7 +9,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 sudo su
 echo "deb https://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list
 apt-get update
-sudo apt-get install virtualbox virtualbox-ext-pack
+sudo apt-get install -y virtualbox virtualbox-ext-pack
 
 #Install Kubectl
 sudo apt-get update && sudo apt-get install -y apt-transport-https
@@ -18,10 +18,13 @@ sudo touch /etc/apt/sources.list.d/kubernetes.list
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubectl
 kubectl version
+sudo apt install -y docker.io
+sudo usermod -aG docker $USER && newgrp docker
 
 # Install minikube
 sudo apt update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-comment
+
