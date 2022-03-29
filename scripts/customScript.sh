1c13878bb2f55fe36a6106371d82f1540e2648be
aravindan-acct <aravindan.acct@gmail.com>
2022-03-29 14:14:31 +0530

    Update customScript.sh

--- scripts/customScript.sh
+++ scripts/customScript.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+#Install VirtualBox hypervisor
+
+sudo apt-get update
+sudo apt remove virtualbox*
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo su
+echo "deb https://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list
+apt-get update
+sudo apt-get install virtualbox virtualbox-ext-pack
+
+#Install Kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y kubectl
+kubectl version
+
+# Install minikube
 sudo apt update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
