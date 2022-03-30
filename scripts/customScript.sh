18fd16215e237ad48efeb77db18c15ced9af38e4
aravindan <aravindan.acct@gmail.com>
2022-03-30 17:43:26 +0530

    minikube configuration updates

--- scripts/customScript.sh
+++ scripts/customScript.sh
@@ -2,6 +2,7 @@
 
 sudo apt update -y
 sudo apt install -y curl wget apt-transport-https
+sudo apt install jq -y
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo cp minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod +x /usr/local/bin/minikube
@@ -14,3 +15,46 @@ echo "installing docker"
 sudo apt install -y docker.io
 echo "adding labuser to docker group"
 sudo usermod -aG docker labuser && newgrp docker
+echo "starting minikube"
+minikube start --driver=docker
+#export minikube_interface=`ifconfig | grep "br-" | awk {'print $1'} | tr -d ':'`
+#export minikube_ip=`ifconfig $minikube_interface | grep "inet " | awk {'print $2'} | xargs`
+export minikube_ip=`minikube ip|xargs`
+sudo apt-get -y install nginx
+sudo cp .minikube/ca.key /etc/nginx/cert.key
+sudo cp .minikube/ca.crt /etc/nginx/cert.file
+# NGINX Configuration
+{
+cat > nginxconfig.conf << EOF
+server {
+    
+    listen 80;
+    listen 8443 default ssl;
+    server_name minikube.cudanet.com;
+    ssl_certificate           /etc/nginx/cert.file;
+    ssl_certificate_key       /etc/nginx/cert.key;
+    ssl_session_cache  builtin:1000  shared:SSL:10m;
+    ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4;
+    ssl_prefer_server_ciphers on;
+    access_log            /var/log/nginx/minikube.access.log;
+    location / {
+      proxy_set_header        X-Real-IP \$remote_addr;
+      proxy_set_header        X-Forwarded-For \$remote_addr;
+      proxy_set_header        X-Forwarded-Proto \$scheme;
+      # Fix the “It appears that your reverse proxy set up is broken" error.
+      proxy_pass          https://kubeip:8443;
+      proxy_read_timeout  90;
+      proxy_redirect      https://kubeip:8443 https://minikube.cudanet.com:8443;
+      
+    }
+  }
+
+EOF
+}
+sed -i "s/kubeip/$minikube_ip/g" nginxconfig.conf
+sudo cp nginxconfig.conf nginxconfig.conf.bak
+sudo mv nginxconfig.conf /etc/nginx/sites-enabled/default
+sudo systemctl enable nginx
+sudo systemctl stop nginx
+sudo systemctl start nginx
