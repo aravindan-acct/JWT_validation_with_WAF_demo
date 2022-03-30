0fa705b05a5855bc5b37052b077a3546ac375d9d
aravindan <aravindan.acct@gmail.com>
2022-03-30 20:20:13 +0530

    fixed custom script for minikube

--- scripts/customScript.sh
+++ scripts/customScript.sh
@@ -22,47 +22,16 @@ minikube start --driver=docker
 sudo apt-get -y install nginx
 sudo cp /home/labuser/.minikube/ca.key /etc/nginx/cert.key
 sudo cp /home/labuser/.minikube/ca.crt /etc/nginx/cert.file
+export minikube_ip=`minikube ip|xargs`
+wget https://raw.githubusercontent.com/aravindan-acct/JWT_validation_with_WAF_demo/main/scripts/minikube.conf
 echo "========= exiting as labuser and continuing with the rest of the script ========="
+echo "========= replacing kubeip string with the minikube ip ========="
+sed -i "s/kubeip/$minikube_ip/g" minikube.conf
 EOF
-
 #export minikube_interface=`ifconfig | grep "br-" | awk {'print $1'} | tr -d ':'`
 #export minikube_ip=`ifconfig $minikube_interface | grep "inet " | awk {'print $2'} | xargs`
-export minikube_ip=`minikube ip|xargs`
-
-# NGINX Configuration
-{
-cat > nginxconfig.conf << EOF
-server {
-    
-    listen 80;
-    listen 8443 default ssl;
-    server_name minikube.cudanet.com;
-    ssl_certificate           /etc/nginx/cert.file;
-    ssl_certificate_key       /etc/nginx/cert.key;
-    ssl_session_cache  builtin:1000  shared:SSL:10m;
-    ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4;
-    ssl_prefer_server_ciphers on;
-    access_log            /var/log/nginx/minikube.access.log;
-    location / {
-      proxy_set_header        X-Real-IP \$remote_addr;
-      proxy_set_header        X-Forwarded-For \$remote_addr;
-      proxy_set_header        X-Forwarded-Proto \$scheme;
-      # Fix the “It appears that your reverse proxy set up is broken" error.
-      proxy_pass          https://kubeip:8443;
-      proxy_read_timeout  90;
-      proxy_redirect      https://kubeip:8443 https://minikube.cudanet.com:8443;
-      
-    }
-  }
-
-EOF
-}
-echo "========= replacing kubeip string with the minikube ip ========="
-sed -i "s/kubeip/$minikube_ip/g" nginxconfig.conf
-
-sudo cp nginxconfig.conf nginxconfig.conf.bak
-sudo mv nginxconfig.conf /etc/nginx/sites-enabled/default
+sudo cp /home/labuser/minikube.conf nginxconfig.conf.bak
+sudo mv /home/labuser/minikube.conf /etc/nginx/sites-enabled/default
 echo "========= starting nginx ========="
 sudo systemctl enable nginx
 sudo systemctl stop nginx
