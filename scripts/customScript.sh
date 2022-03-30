4140c090bc0cae28974f170703fcdc81f90ec4fa
aravindan <aravindan.acct@gmail.com>
2022-03-30 19:51:13 +0530

    minikube script modified

--- scripts/customScript.sh
+++ scripts/customScript.sh
@@ -7,27 +7,28 @@ wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd6
 sudo cp minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod +x /usr/local/bin/minikube
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-echo "permissions set for kubectl"
+echo "========= permissions set for kubectl ========="
 chmod +x kubectl
-echo "moving kubectl to /usr/local/bin"
+echo "========= moving kubectl to /usr/local/bin ========="
 sudo mv kubectl /usr/local/bin/
-echo "installing docker"
+echo "========= installing docker ========="
 sudo apt install -y docker.io
-echo "adding labuser to docker group"
+echo "========= adding labuser to docker group ========="
 sudo usermod -aG docker labuser && newgrp docker
-echo "starting minikube"
+echo "========= starting minikube ========="
 sudo -i -u labuser << EOF
-echo "starting minikube as labuser"
+echo "========= starting minikube as labuser ========="
 minikube start --driver=docker
-echo "exiting as labuser and continuing with the rest of the script"
+sudo apt-get -y install nginx
+sudo cp /home/labuser/.minikube/ca.key /etc/nginx/cert.key
+sudo cp /home/labuser/.minikube/ca.crt /etc/nginx/cert.file
+echo "========= exiting as labuser and continuing with the rest of the script ========="
 EOF
 
 #export minikube_interface=`ifconfig | grep "br-" | awk {'print $1'} | tr -d ':'`
 #export minikube_ip=`ifconfig $minikube_interface | grep "inet " | awk {'print $2'} | xargs`
 export minikube_ip=`minikube ip|xargs`
-sudo apt-get -y install nginx
-sudo cp .minikube/ca.key /etc/nginx/cert.key
-sudo cp .minikube/ca.crt /etc/nginx/cert.file
+
 # NGINX Configuration
 {
 cat > nginxconfig.conf << EOF
@@ -57,9 +58,12 @@ server {
 
 EOF
 }
+echo "========= replacing kubeip string with the minikube ip ========="
 sed -i "s/kubeip/$minikube_ip/g" nginxconfig.conf
+
 sudo cp nginxconfig.conf nginxconfig.conf.bak
 sudo mv nginxconfig.conf /etc/nginx/sites-enabled/default
+echo "========= starting nginx ========="
 sudo systemctl enable nginx
 sudo systemctl stop nginx
 sudo systemctl start nginx
